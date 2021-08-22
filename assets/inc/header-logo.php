693f25fd645ce22c908fd9a2124f0d1fa11a1931
Erhan ÜRGÜN <erhan.rgn04@gmail.com>
2021-08-23 00:18:21 +0300

    front-end - ayarlarda değişiklikler yapıldı

---
+++ assets/inc/header-logo.php
@@ -0,0 +1,26 @@
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="60px" height="50px" viewBox="293.838 0 156.662 109.5" enable-background="new 293.838 0 156.662 109.5" xml:space="preserve">
+    <g id="_x3C_ikon-golge_x3E_">
+        <g id="kesit-sag_1_">
+            <polygon fill="#04091E" points="407.687,105.093 426.189,80.748 444.692,56.404 407.687,56.404 		" />
+        </g>
+        <g id="kesit-orta_1_">
+            <polygon fill="#04091E" points="407.687,105.093 389.184,80.751 370.681,56.408 370.681,105.093 333.674,105.093 333.674,7.727 
+			370.638,56.408 370.641,7.724 407.687,7.727 		" />
+        </g>
+        <g id="kesit-sol_1_">
+            <polygon fill="#04091E" points="296.667,56.404 333.674,56.404 333.674,7.726 315.168,32.068 		" />
+        </g>
+    </g>
+    <g id="_x3C_ikon-sari_x3E_">
+        <g id="kesit-sag">
+            <polygon fill="var(--main-color-1)" points="409.66,102.463 428.163,78.117 446.667,53.778 409.66,53.778 		" />
+        </g>
+        <g id="kesit-orta">
+            <polygon fill="#FFFFFF" points="409.66,102.464 391.156,78.117 372.654,53.778 372.654,102.464 335.647,102.464 335.647,5.094 
+			372.611,53.778 372.613,5.093 409.66,5.094 		" />
+        </g>
+        <g id="kesit-sol">
+            <polygon fill="var(--main-color-1)" points="298.638,53.778 335.647,53.778 335.647,5.094 317.142,29.437 		" />
+        </g>
+    </g>
+</svg>
--- header.php
+++ header.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Erhan ÜRGÜN, @erhanurgun">
     <meta name="description" content="E-Ticaret sitesi için bir sepet uyulamması örneği">
-    <title>PHP ve jQuery ile Sepetim Uygulaması</title>
     <link rel="shortcut icon" href="https://bilinentasarim.com/dimg/diger/favicon.svg">
     <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.min.css">
@@ -16,8 +15,28 @@
 
     <style>
         :root {
-            --main-color-1: #fab700;
-            --main-color-2: #212529;
+            /* DeepYellow */
+            --color-1: #fab700;
+            /* DeepPink */
+            --color-2: #ff1493;
+            /* DeepGreen */
+            --color-3: #04aa6d;
+            /* Dark */
+            --color-4: #212529;
+            /* FireBrick */
+            --color-5: #b22222;
+            /* DodgerBlue */
+            --color-6: #1e90ff;
+            /* OrangeRed */
+            --color-7: #ff4500;
+            /* Purple */
+            --color-8: #800080;
+            /* White */
+            --color-9: #ffffff;
+            /* Main Color 1 */
+            --main-color-1: var(--color-1);
+            /* Main Color 2 */
+            --main-color-2: var(--color-4);
         }
     </style>
 </head>
@@ -28,7 +47,9 @@
         <nav class="navbar navbar-expand-lg navbar-dark bt-dark">
             <div class="container">
                 <a class="navbar-brand" href="index">
-                    <img width="50" src="https://bilinentasarim.com/dimg/diger/favicon.svg" alt="BT Icon">
+                    <span class="logo">
+                        <?php require_once 'assets/inc/header-logo.php'; ?>
+                    </span>
                     <strong class="market"><span class="bt-text">BT</span>Market</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#btNavbar" aria-controls="btNavbar" aria-expanded="false" aria-label="Toggle navigation">
